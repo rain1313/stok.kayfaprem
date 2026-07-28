2bb9d1696d444301a53adc22e8338305e263f9c7
rain1313 <rifkyolindafebrian@gmail.com>
2026-07-28 15:54:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 ========================================= */
 
 const API_URL =
-    "https://script.google.com/macros/s/AKfycbwEu48llXaE4KJMl_8GoehxE1OEk4LRClgNS-t-ffY4atCSObg77vKXRB9CvE0XIgDV/exec";
+    "https://script.google.com/macros/s/AKfycbzjmPVAmHmPnXZ3WOJEJZjp5Az7AzqQIHzkpupcVyJX1tB6k1iEo31DbfTXVG5IaOnT/exec";
 
 
 /* =========================================
